bff9d14a017484538a69b61032dbf49f283d61ac
Jay Chen <jc01@berkeley.edu>
2023-04-13 12:46:58 -0700

    Update

--- currency.py
+++ currency.py
@@ -6,13 +6,11 @@ import datetime
 
 # Get the link of the website
 req = requests.get("https://www.google.com/finance/quote/USD-CNY?sa=X&ved=2ahUKEwiD3_b04df9AhU6FzQIHdXoDpcQmY0JegQIBhAd")
-
 # Web scraper tool
 soup = BeautifulSoup(req.content, "html.parser")
 
 # Get current date and time
 now1 = datetime.datetime.now()
-
 # Get current month's abbreviation
 month_abbr = now1.strftime("%b")
 
@@ -21,7 +19,6 @@ text = soup.get_text().split()
 
 # Requirement of keyword
 pattern = re.compile(r'^CNY(\d+\.\d+)' + month_abbr)
-
 # For loop to find the specific string satifies the pattern
 for string in text:
     match = pattern.search(string)
@@ -34,45 +31,35 @@ def ask():
     # Get the latest date and time
     now = datetime.datetime.now()
     time = now.strftime("%m/%d/%Y %H:%M:%S")
-    print("Currency: " + str(var) + "\n" + time + "\n")
-
-    choice = int(input("'1' start with latest ratio, '2' use different ratio:, '3' to exit: "))
-    if choice == 1:
-        calculate()
-    elif choice == 2:
-        calculate2()
-    else:
+    print("USD to RMB: " + str(var) + "\n" + time + "\n" + "\n")
+    # Ask for choice
+    choice = int(input("'1' use latest ratio, '2' use different ratio, '3' exit: "))
+    if choice == 1 or choice == 2:
+        calculate(choice)
+    elif choice == 3:
         return
+    else:
+        print("Invalid input")
 
-def calculate():
+def calculate(choice):
+    if choice == 1:
+        ratio = var
+    else:
+        ratio = float(input("Enter the ratio: "))
+    # rmb->usd or usd to rmb
     way = int(input("'1': rmb -> usd, '2': usd -> rmb: "))
-    ratio = var
-    amount = input("Enter the amount: ")
     if way == 1:
-        print("Current ratio: " + var)
-        print("Amount of $", round((float(amount) / float(ratio)), 4))
+        amount = float(input("Enter the amount :¥ "))
+        print("Amount of USD: $", round((float(amount) / float(ratio)), 4))
         print("\n")
         print("\n")
     elif way == 2:
-        print("Current ratio: " + var)
-        print("Amount of ¥", round((float(amount) * float(ratio)), 4))
+        amount = float(input("Enter the amount :$ "))
+        print("Amount of RMB: ¥", round((float(amount) * float(ratio)), 4))
         print("\n")
         print("\n")
+    else:
+        print("Invalid input")
     ask()
-
-def calculate2():
-    way = int(input("'1': rmb -> usd, '2': usd -> rmb: "))
-    ratio = input("Enter the ratio: ")
-    amount = input("Enter the amount: ")
-    if way == 1:
-        print("Amount of $", round((float(amount) / float(ratio)), 4))
-        print("\n")
-        print("\n")
-    elif way == 2:
-        print("Amount of ¥", round((float(amount) * float(ratio)), 4))
-        print("\n")
-        print("\n")
-    ask()
-
 
 ask()
