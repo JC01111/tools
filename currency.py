8bddb69511a21ae06b33f61fc90846ba0aa9f601
Jay Chen <jc01@berkeley.edu>
2023-07-19 23:11:05 -0700

    Update

--- currency.py
+++ currency.py
@@ -8,14 +8,12 @@ import datetime
 now1 = datetime.datetime.now()
 # Get current month's abbreviation
 month_abbr = now1.strftime("%b")
-
 # Get the link of the website
 req = requests.get("https://www.google.com/finance/quote/USD-CNY?sa=X&ved=2ahUKEwiD3_b04df9AhU6FzQIHdXoDpcQmY0JegQIBhAd")
 # Web scraper tool
 soup = BeautifulSoup(req.content, "html.parser")
 # First get the text of the web page, then split the strings with ""
 text = soup.get_text().split()
-
 # Requirement of keyword
 pattern = re.compile(r'^CNY(\d+\.\d+)' + month_abbr)
 # For loop to find the specific string satifies the pattern
@@ -49,14 +47,10 @@ def calculate(choice):
     way = int(input("'1': rmb -> usd, '2': usd -> rmb: "))
     if way == 1:
         amount = float(input("Enter the amount :¥ "))
-        print("Amount of USD: $", round((float(amount) / float(ratio)), 4))
-        print("\n")
-        print("\n")
+        print(f"Amount of USD: $ {round((float(amount) / float(ratio)), 4)}\n\n")
     elif way == 2:
         amount = float(input("Enter the amount :$ "))
-        print("Amount of RMB: ¥", round((float(amount) * float(ratio)), 4))
-        print("\n")
-        print("\n")
+        print(f"Amount of RMB: ¥ {round((float(amount) * float(ratio)), 4)}\n\n")
     else:
         print("Invalid input")
     ask()
